8c341096497e2ef2621c291037e270c023420690
Tóth Kevin <tothkevin0716@gmail.com>
2024-04-12 14:30:19 +0200

    Created endpoints inside game controller

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
-import { GameService } from './game.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Coordinates, GameService } from './game.service';
 
 @Controller('/game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}
+
+  @Get('/:sessionId')
+  async getGameState(@Param('sessionId') sessionId: string) {
+    return this.gameService.getGameState(sessionId);
+  }
+
+  @Post('/:sessionId/play')
+  async postMove(
+    @Param('sessionId') sessionId: string,
+    @Body() coordinates: Coordinates,
+  ) {
+    return this.gameService.setGameBoard(sessionId, coordinates);
+  }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
+export interface Coordinates {
+  x: number;
+  y: number;
+}
+
 @Injectable()
 export class GameService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async getGameState(sessionId: string) {}
+
+  async setGameBoard(sessionId: string, coordinates: Coordinates) {}
 }
